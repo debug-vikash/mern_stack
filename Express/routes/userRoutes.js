05ac7577c6231e2c8c5fd0ebb14382ea58a5cf26
debug-vikash <vikashmaurya861@gmail.com>
2026-03-16 17:44:34 +0530

    Post Done

--- Express/routes/userRoutes.js
+++ Express/routes/userRoutes.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = express.Router()
 const userController = require('../controllers/userController')
 
-router.get("/users", userController.getAllUsers);
+router.get("/users", userController.getUsers);
+
+router.post("/add/users", userController.addUsers);
 
 module.exports = router
--- Express/server.js
+++ Express/server.js
@@ -3,6 +3,8 @@ const userRoutes = require('./routes/userRoutes')
 
 const app = express()
 
+app.use(express.json())
+
 app.use("/", userRoutes)
 
 app.listen(3000, () => {
