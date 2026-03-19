62f3e1a9d1d73741b3e50ad76f7ba53b2495dbca
debug-vikash <vikashmaurya861@gmail.com>
2026-03-19 11:53:01 +0530

    Custom Middleware

--- Express/modules/productModules.js
+++ Express/modules/productModules.js
@@ -1,7 +1,9 @@
 const products = [
     {
-        id: 1,
-        name: "Product 1",
-        price: 100
+        "id": "1",
+        "name": "Product 1",
+        "price": 100
     }
 ]
+
+module.exports = products
--- Express/server.js
+++ Express/server.js
@@ -1,9 +1,28 @@
 const express = require('express')
 const userRoutes = require('./routes/userRoutes')
 const productRoutes = require('./routes/productRoutes')
-
 const app = express()
 
+//Custom middleware for logging requests
+//-->
+const userMiddleware = (req, res, next) => {
+    // Middleware logic for user authentication or other checks
+    console.log("User middleware executed", req.query.skip);
+    if(req.query.skip === "true") {
+        next();
+    }else{
+        res.send("Unortherised user");
+    }
+
+    //If fails authentication, you can send a response like this:
+    //res.send("Unauthorized access");
+
+    // If successful, call next() to proceed to the next middleware or route handler
+    //next();
+};
+
+
+app.use(userMiddleware);
 app.use(express.json())
 
 app.use("/", userRoutes)
